888d6e8df8c25819922293ff3ef4007a44390ba1
Rafael Janetzek <rjanetzek@gmx.de>
2018-02-13 17:37:53 +0100

    removed unsused styles, preparation for hushlist messenger

--- src/renderer/components/Messenger/Menu.vue
+++ src/renderer/components/Messenger/Menu.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    <div>
-     
-    </div>
-    <div class="inner-content">
-      <router-view></router-view>
-    </div>
-  </div>    
+    <el-container>
+      <el-aside width="300px" >
+        <div class="messenger-title"> Messenger</div>
+        <div>
+          <div class="subtitle"> Lists </div>  
+          <div class="subtitle"> Groups </div> 
+          <div class="subtitle"> Conversations </div> 
+        </div>
+      </el-aside>
+      <el-main> 
+        <router-view></router-view>
+      </el-main>
+    </el-container>    
 </template>
 
 <script>
@@ -66,384 +71,37 @@
     font-family: 'Poppins', sans-serif;
     color: #2d2d2d;
   }
+    
+  .messenger-title {
+    margin-top: 3px;
+    font-size: 15pt;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: white;
+    padding: 15px 10px 10px 15px;
+    width:100%;
+    border-bottom: 1px solid white
+  }
 
-  #wrapper {
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );
+    .subtitle {
+    margin-top: 3px;
+    font-size: 12pt;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: white;
+    padding: 15px 10px 10px 15px;
+    width:100%;
+  }
+
+  .el-container {
+    margin-left:68px;
     height: 100vh;
-    width: 100vw;
-    padding: 0px;
   }
 
-  #logo {
-    float: left;
-    height: 100px;
-    margin-bottom: 40px;
-    width: auto;
-  }
-
-  #logo-text {
-    float: left;
-    margin-left: 20px;
-    line-height: 100px;
-    font-size: 18pt;
-    font-weight: bold;
-  }
-
-  main {
-    clear: left;
-    display: flex;
-    justify-content: space-between;
-  }
-
-  main > div { flex-basis: 50%; }
-
-  .left-side {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
-
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .install-list {
-    color: #555;
-    font-weight: initial;
-    list-style-type: none;
-    margin-bottom: 50px;
-  }
-
-  .intall-list li {
-    color: #000;
-    height: 20px;
-  }
-
-  .install-list li code {
-    color: #3e3e3e;
-    font-weight: bold;
-  }
-
-  .progress {
-    float: left;
-    width: 10px;
-    height: 10px;
-    margin-top: 6px;
-    margin-right: 5px;
-    border-radius: 50%;
-    background-color: #d1d1d1;
-  }
-
-  #connecting {
-    text-align: center;
-    line-height: 80vh;
-    font-size: 1.2em;
-  }
-  .pending {
-    border: 2px solid #7ed35f;
-    background-color: transparent;
-  }
-
-  .success {
-    background-color: #7ed35f;
-  }
-
-  .error {
-    background-color: #ef4049;
-  }
-
-  .doc .button {
-    font-size: .9em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #2F77F7;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #2F77F7;
-    margin-top: 10px;
-    text-decoration: none;
-    -webkit-app-region: no-drag;
-  }
-
-  .doc .button:hover {
-    background-color: #2262d6;
-  }
-
-  .doc .button-alt {
-    color: #3e3e3e;
-    margin-right: 5px;
-    background-color: transparent;
-  }
-
-  .doc .button-alt:hover {
-    background-color: #e2e2e2;
-  }
-
-  .inner-content {
-    position: absolute;
-    width: 100%;
+  .el-aside {
     height: 100%;
-    padding: 77px 20px 20px 88.3px;
-  }
-
-  .bottom-row {
-    clear: both;
-    position: fixed;
-    bottom: 15px;
-  }
-
-  .bottom-row .box {
-    float: left;
-    width: 300px;
-    height: 95px;
-    margin-right: 15px;
-    padding: 10px 15px 10px 15px;
-    background-color: #3e3e3e;
-  }
-
-  .bottom-row .alt {
-    font-weight: 300;
-    font-size: 11pt;
-    background-color: #cacaca;
-  }
-
-  .bottom-row .box .links {
-    position: absolute;
-    bottom: 10px;
-  }
-
-  .bottom-row .box .links a {
-    text-decoration: none;
-    cursor: pointer;
-    -webkit-app-region: no-drag;
-  }
-
-  .bottom-row .box #texts {
-    float: left;
-    list-style-type: none;
-  }
-
-  .bottom-row .box #balances {
-    float: right;
-    text-align: right;
-    list-style-type: none;
-  }
-
-  .bottom-row .box li {
-    font-weight: 500;
-    color: #fff;
-  }
-
-  .container {    
-    width: 100%;
-    margin-top: 10px;
-    padding: 15px 25px 15px 30px;
-    background-color: #eaeaea;
-    border-radius: 11px;
-  }
-  
-  .caption {
-    font-weight: 700;
-    font-size: 12pt
-  }
-  
-  .caption .balance {
-    font-weight: 400;
-  }
-
-  .caption span {
-    font-weight: 400;
-    color: #2f77f7;
-  }
-
-  .intro {
-    font-weight: 400;
-    font-size: 10pt
-  }
-  .copy {    
-    font-weight: 400;
-    font-size: 11pt;
-    color: #5e5e5e;
-  }
-
-  .el-table__row .address .cell {
-    font-family: 'Courier', sans-serif;
-  }
-
-  .el-table__row .balance .cell {
-    font-family: 'Courier', sans-serif;
-    color: #2f77f7;
-  }
-
-  .el-table td, .el-table th {
-    padding: 4px 0;
-  }
-
-  .el-table__body-wrapper, .el-table__footer-wrapper, .el-table__header-wrapper {
-    background-color:#eaeaea;
-  }
-
-  el-table__body, .el-table__footer, .el-table__header {    
-      background-color:#eaeaea;
-  }
-
-  .el-table .gutter {
-        background-color:#eaeaea;
-  }
-
-  .el-table td, .el-table th.is-leaf {
-      background-color:#eaeaea;
-      border: none;
-  }
-
-  .button {
-    font-size: 11pt;
-    cursor: pointer;
-    outline: none;
-    padding: 5px 15px 5px 15px;
-    border-radius: 4px;
-    display: inline-block;
-    color: #fff;
-    background-color: #2F77F7;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #2F77F7;
-    text-decoration: none;
-    -webkit-app-region: no-drag;
-  }
-
-  .button:hover {
-    background-color: #2262d6;
-  }
-
-  .button-alt {
-    color: #3e3e3e;
-    margin-right: 5px;
-    background-color: transparent;
-  }
-
-  .button-alt:hover {
-    background-color: #e2e2e2;
-  } 
-  
-  div#wallet-menu {
-    position: absolute;
-    width: 100vw;
-    background-color: #fff;
-    padding: 10px 10px 10px 88.3px;
-    border-bottom: 2px solid #e3e3e3;
-    z-index: 1;
-  }
-
-  .menu-title {
-    float: left;
-    margin-top: 3px;
-    font-size: 15pt;
-    font-weight: 700;
-    text-transform: uppercase;
-  }
-
-  .block-text {
-    position: fixed;
-    right: 40px;
-    margin: 4px 15px 0px 0px;
-  }
-
-  .chain-data {
-    float: right;
-    margin-top: 4px;
-    margin-right: 40px;
-    font-size: 12pt;
-    font-weight: 300;
-    text-transform: uppercase;
-    list-style-type: none;
-  }
-  
-  div#wallet-menu {
-    position: absolute;
-    width: 100vw;
-    background-color: #fff;
-    padding: 10px 10px 10px 88.3px;
-    border-bottom: 2px solid #e3e3e3;
-    z-index: 1;
-  }
-
-  .menu-title {
-    float: left;
-    margin-top: 3px;
-    font-size: 15pt;
-    font-weight: 700;
-    text-transform: uppercase;
-  }
-
-  .block-text {
-    position: fixed;
-    right: 40px;
-    margin: 4px 15px 0px 0px;
-  }
-
-  .chain-data {
-    float: right;
-    margin-top: 4px;
-    margin-right: 40px;
-    font-size: 12pt;
-    font-weight: 300;
-    text-transform: uppercase;
-    list-style-type: none;
-  }
-
-  .chain-data li {
-    margin-left: 15px;
-    display: inline;
-  }
-
-  .wallet-sections {
-    float: left;
-    margin-left: 30px;
-    margin-top: -10px;
-    margin-bottom: -11px;
-    height: 56px;
-    -webkit-app-region: no-drag;
-  }
-
-  .wallet-sections li {
-    list-style-type: none;
-    display: inline-block;
-    font-size: 12pt;
-    font-weight: 500;
-    text-transform: uppercase;
-    height: 100%;
-    line-height: 56px;
-  }
-
-  .wallet-sections li:hover {
-    cursor: pointer;
-    background-color: #e2e2e2;
-  }
-
-  .wallet-sections .active {
-    border-bottom: 4px solid #cacaca;
+    min-height:100%;
+    background-color: #A1A1A1;
+    color: #ABABAB;    
   }
 </style>
