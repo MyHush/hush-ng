6dfded9bc330fca1927ac0ee16a342a3f948794a
Jonathan "Duke" Leto <jonathan@leto.net>
2018-04-15 14:42:13 -0700

    Dev fee should apply to total amount being sent and ignore network fee

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -625,16 +625,19 @@ export default new Vuex.Store({
         return;
       }
 
-      var dev_fee      = transactionForm.amount * 0.01;
-      var receivers    = [{
-        // Wallet Support Fee, the maintenance of development of this wallet depends on this
+      var num_destinations   = transactionForm.destinationAddresses.length;
+      var transaction_amount = transactionForm.amount * num_destinations;
+      // This is 1% of total amount being sent, ignoring network fee
+      var dev_fee            = 0.01 * transaction_amount;
+      var receivers          = [{
+        // Wallet Support Fee, the maintenance of development of this wallet depends on this!!! :)
         // Thanks For Supporting Hush-NG!
 		"address": "zcU6yx5eUXqcDjeT5NJnhgEdVVrt2fCrdCGFGkWbNgcdq11XKUgsDjMErxUvnvFsSwAxXrGfaiqsY4L4gJ8RYmBfrEZvHLb",
         // 1% of zaddr xtns for sustainable wallet support, maintenance and development
 		"amount":  dev_fee,
         // Feel free to modify this to send your feedback, if you are mucking about in the code :)
 		"memo":    encodeMemo("Hush-NG Rocks!")
-	  }];         
+	  }];
 	  var memo         = transactionForm.memo;
 	  var encoded_memo = encodeMemo(memo);
       var total_amount = transactionForm.fee + dev_fee;
@@ -653,13 +656,13 @@ export default new Vuex.Store({
 
       for(let receiver of transactionForm.destinationAddresses) {
         receivers.push({
-		"address": receiver.toString(),
-		"amount":  transactionForm.amount,
-		"memo":    encoded_memo
-		});
+            "address": receiver.toString(),
+            "amount":  transactionForm.amount,
+            "memo":    encoded_memo
+        });
         // TODO: maybe support diff amounts to diff addresses?
         total_amount += transactionForm.amount;
-      }       
+      }
 
         var current_balance = await client.getBalance();
         if (current_balance >= total_amount) {
